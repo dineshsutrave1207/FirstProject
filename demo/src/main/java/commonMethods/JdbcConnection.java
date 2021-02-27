105017e29c165ce2efa67f955a792310bd124631
dinesh <dinesh@DESKTOP-99IPKMK>
2021-02-27 11:44:29 +0530

    changes in Jdbc Connection

--- demo/src/main/java/commonMethods/JdbcConnection.java
+++ demo/src/main/java/commonMethods/JdbcConnection.java
@@ -4,15 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
+import utilities.PropertyHandler;
 public class JdbcConnection {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub  
 		try
 		{
-			Class.forName("oracle.jdbc.OracleDriver"); 
-			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","admin");  
+			String query="select * from employee where company='tcs'";
+			PropertyHandler.loadpropertyfile("qaconfig");
+			Class.forName(PropertyHandler.getProperty("dbconnectionclassname"));
+			Connection con=DriverManager.getConnection(PropertyHandler.getProperty("dbconnection"),PropertyHandler.getProperty("dbusername"),PropertyHandler.getProperty("dbpassword"));  
 			Statement stmt=con.createStatement();  
-			ResultSet rs1=stmt.executeQuery("select * from employee where company='tcs'");
+			ResultSet rs1=stmt.executeQuery(query);
 			while(rs1.next()) 
 			{
 				System.out.println(rs1.getString(1)+"  "+rs1.getString(2)+"  "+rs1.getString(3)); 
